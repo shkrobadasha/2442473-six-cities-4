5bb0e269cb23d39e6d83ba41f4adef260a511028
shkrobadasha <shkrobadaria@mail.ru>
2024-06-06 15:29:54 +0300

    fix-markers

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -16,13 +16,13 @@ type mapData = {
 
 const defaultCustomIcon = new Icon({
   iconUrl: URL_MARKER_DEFAULT,
-  iconSize: [40, 40],
+  iconSize: [30, 40],
   iconAnchor: [20, 40]
 });
 
 const currentCustomIcon = new Icon({
   iconUrl: URL_MARKER_CURRENT,
-  iconSize: [40, 40],
+  iconSize: [30, 40],
   iconAnchor: [20, 40]
 });
 
